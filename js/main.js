adbac55dac779e3b77fa7593e654af215ff44ef2
katherine montecinos <katherine.montecinos.k@gmail.com>
2016-10-25 22:30:42 -0300

    falta aun validar lista de bicis y letra mayuscula en nombre y apellido

---
+++ js/main.js
@@ -0,0 +1,32 @@
+function validateForm(){
+	var inputname = document.getElementById("name").value;
+	var inputlastname = document.getElementById("lastname").value;
+	var inputemail = document.getElementById("input-email").value;
+	var inputpassword = document.getElementById("input-password").value;
+	var listabici = document.getElementsByClassName("form-control").selectedIndex;
+
+	if( inputname == null || inputname.length == 0 || /^\s+$/.test(inputname) || /^[a-zA-Z]*$/.test(inputname) == false ) {
+ 		alert("Debe ingresar un nombre");
+ 		return false;
+ 	}
+
+ 	else if( inputlastname == null || inputlastname.length == 0 || /^\s+$/.test(inputlastname) || /^[a-zA-Z]*$/.test(inputname) == false ) {
+ 		alert("Debe ingresar un apellido");
+ 		return false;
+ 	}
+
+ 	else if( inputemail == null || inputemail.length == 0 || /^\s+$/.test(inputemail) || /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/.test(inputemail) == false ) {
+ 		alert("Debe ingresar un email valido");
+ 		return false;
+ 	}
+ 	else if( inputpassword == null || inputpassword  == 0 || /^\s+$/.test(inputpassword) || inputpassword.length < 6 || /123456/.test(inputpassword) == true || /098754/.test(inputpassword) || /password/.test(inputpassword)) {
+ 		alert("Debe ingresar un password valido");
+ 		return false;
+ 	}
+ 	else if( listabici == null || listabici == 0 ){
+ 		alert("Debes seleccionar una opción");
+ 		return false;
+ 	}
+ 	return true;
+}
+    
